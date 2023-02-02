33efa4370a6ac8123ebe5e4e251bcb3726b72aee
hyebeen byun <hyebeen2658@naver.com>
2023-02-02 14:20:55 +0900

    [setting] bindEvent 수정:wrench:

--- client/lib/dom/bindEvent.js
+++ client/lib/dom/bindEvent.js
@@ -1,20 +1,22 @@
-
 import { getNode } from "./getNode.js";
 import { typeError } from "../error/index.js";
 
-
-export function bindEvent(node,type,handler){
-  if(typeof node === 'string'){
+export function bindEvent(node, type, handler) {
+  if (typeof node === "string") {
     node = getNode(node);
   }
 
-  if(!(/mouseenter|click|mousemove|mouseleave/g.test(type))){
-    typeError('bindEvent 함수의 두 번째 인자는 유효한 이벤트 타입 이어야 합니다.')
+  if (!/mouseenter|mouseover|mouseout|click|mousemove|mouseleave/g.test(type)) {
+    typeError(
+      "bindEvent 함수의 두 번째 인자는 유효한 이벤트 타입 이어야 합니다."
+    );
   }
 
+  if (type.includes(" ")) {
+    type.split(" ").forEach((item) => node.addEventListener(item, handler));
+  }
 
-  node.addEventListener(type,handler);
-  
-  return ()=> node.removeEventListener(type,handler);
+  node.addEventListener(type, handler);
 
+  return () => node.removeEventListener(type, handler);
 }
