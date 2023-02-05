126f26dbe52b0561e7c998ab5bb499acfb8d7096
bellaru2022 <rkddls0918@naver.com>
2023-02-05 22:55:46 +0900

    [feat] 보화 main js파일 :sparkles:

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,75 @@
-let target = document.querySelectorAll('.img-button'); 
-let btnPopClose = document.querySelectorAll('.addbtn .cancel, .addbtn .purple'); 
+// 상단 스와이퍼 기능
+window.addEventListener('load', async () => {
+  const swiper = new Swiper('.swiper', {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 10,
+      speed: 400,
+      navigation: {
+          nextEl: '.nextBtn',
+          prevEl: '.swiper-button-prev',
+      },
+  });
+
+  document.getElementById('nextbtn').addEventListener('click', () => {
+      swiper.slideNext();
+  })
+
+
+
+
+  // 수량 및 가격변경 기능구현
+
+  const quantityNumber = document.querySelector('.quantity-number');
+  const price = document.querySelector('.quantity-price');
+  const sum = document.querySelector('.sum');
+  document.querySelector('.minusbtn').addEventListener('click', () => {
+      const value = Number(quantityNumber.textContent);
+      const priceValue = Number(price.textContent.replaceAll('원', '').replaceAll(',', ''));
+      if (value > 1) {
+          quantityNumber.textContent = value - 1;
+          sum.textContent = (priceValue * (value - 1)).toLocaleString() + '원';
+      }
+  });
+
+  document.querySelector('.plusbtn').addEventListener('click', () => {
+      const value = Number(quantityNumber.textContent);
+      const priceValue = Number(price.textContent.replaceAll('원', '').replaceAll(',', ''));
+      quantityNumber.textContent = value + 1;
+      sum.textContent = (priceValue * (value + 1)).toLocaleString() + '원';
+  });
+});
+
+
+// 하단 스와이퍼기능
+
+window.addEventListener('load', async () => {
+  const swiper1 = new Swiper('.swiper1', {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 10,
+      speed: 400,
+      navigation: {
+          nextEl: '.nextBtn',
+          prevEl: '.swiper-button-prev',
+      },
+  });
+
+  document.getElementById('nextbtn1').addEventListener('click', () => {
+      swiper1.slideNext();
+  });});
+
+
+
+
+// 장바구니 모양 누르면 팝업창
+
+let target = document.querySelectorAll('.img-button');
+let btnPopClose = document.querySelectorAll('.addbtn .cancel, .addbtn .purple');
 let targetID;
+
+
+
 
 // 팝업 열기
 for(let i = 0; i < target.length; i++){
@@ -10,9 +79,11 @@ for(let i = 0; i < target.length; i++){
   });
 }
 
+
 // 팝업 닫기
 for(let j = 0; j < target.length; j++){
   btnPopClose[j].addEventListener('click', function(){
-    this.parentNode.parentNode.parentNode.parentNode.style.d/isplay = 'none';
+    this.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
   });
-}
+};
+
