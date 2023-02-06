7145ce3ce555fcc737432b1c4e1882c4dc6a89d1
hyebeen byun <hyebeen2658@naver.com>
2023-02-06 23:25:01 +0900

    [chore] 파일 프리티어로 수정 :wrench:

--- client/js/common/bannerSwiper.js
+++ client/js/common/bannerSwiper.js
@@ -1,11 +1,10 @@
-const bannerSwiper = new Swiper('.banner > .swiper',{
-  autoplay:true,
-  loop:true,
+const bannerSwiper = new Swiper(".banner > .swiper", {
+  autoplay: true,
+  loop: true,
   speed: 200,
-  parallax:true,
-  navigation:{
-    nextEl:".swiper-button-next",
-    prevEl:".swiper-button-prev",
-  }
-
-})
+  parallax: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
